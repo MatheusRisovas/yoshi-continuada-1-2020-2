f9518d975418f8f044a0e81143ea5a15d955d0c6
MatheusRisovas <matheusrisovas@gmail.com>
2020-09-06 11:16:19 -0300

    adiciona estrutura inicial do projeto

---
+++ app/src/main/java/com/example/yoshi_continuada_1_2020_2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.yoshi_continuada_1_2020_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
